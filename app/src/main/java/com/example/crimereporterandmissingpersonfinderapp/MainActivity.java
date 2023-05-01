86871df4b3e1a4e6655d5e5ef2f5bbc538f90c22
shahmir-ahmed <112096617+shahmir-ahmed@users.noreply.github.com>
2023-05-01 14:33:35 +0500

    Main Activity Dev.
    Developing Main Activity v1.3

--- app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/MainActivity.java
+++ app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.crimereporterandmissingpersonfinderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button crimesSearchButton;
+    private Button missingPersonsSearchButton;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing UI buttons
+        crimesSearchButton = (Button) findViewById(R.id.crimesSearch);
+        missingPersonsSearchButton = (Button) findViewById(R.id.missingPersonsSearch);
+        loginButton = (Button) findViewById(R.id.login);
+
+        // Adding event listener
+        crimesSearchButton.setOnClickListener(this);
+        missingPersonsSearchButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+
     }
+
+    // When any button is clicked
+    @Override
+    public void onClick(View view) {
+
+        // type cast the clicked view to button
+        Button btn = (Button) view;
+
+        // get the text on the button
+        String text = btn.getText().toString();
+
+        // if crimes search button is clicked
+        if(text.equals("Crimes Around You")){
+            Toast.makeText(this, "Crimes Search Screen Opening!", Toast.LENGTH_SHORT).show();
+        }
+        // if missing persons search button is clicked
+        else if(text.equals("Missing People")){
+            Toast.makeText(this, "Missing People Search Screen Opening!", Toast.LENGTH_SHORT).show();
+        }
+        // if login button is clicked
+        else if(text.equals("Login")){
+            Toast.makeText(this, "Login Screen Opening!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
