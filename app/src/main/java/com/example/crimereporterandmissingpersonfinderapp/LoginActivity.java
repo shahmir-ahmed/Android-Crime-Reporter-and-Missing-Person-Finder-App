2c7fc20de51c61d7014429d9f30b834ca16deaed
shahmir-ahmed <112096617+shahmir-ahmed@users.noreply.github.com>
2023-05-14 15:01:40 +0500

    Login Activity Dev.
    Developing Login Activity v1.2

--- app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/LoginActivity.java
+++ app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/LoginActivity.java
@@ -3,12 +3,61 @@ package com.example.crimereporterandmissingpersonfinderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // edit text fields username and password
+    EditText etUsername, etPassword;
+
+    // login, forgot password and sign up buttons
+    Button btnLogin, btnForgotPassword, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize views
+        etUsername = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnForgotPassword = (Button) findViewById(R.id.btn_forgot_password);
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+
+        // Set click listeners
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform login logic here
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Perform actual login logic here, such as authentication against a server
+                    Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform forgot password logic here
+                Toast.makeText(LoginActivity.this, "Forgot password clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform sign up logic here
+                Toast.makeText(LoginActivity.this, "Sign up clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
