4533a41f46eaea0a09f19ad1e4d0278ca31538a2
shahmir-ahmed <112096617+shahmir-ahmed@users.noreply.github.com>
2023-05-14 17:58:56 +0500

    Report Crime Fragment Dev.
    Developing Report Crime Fragment v1.1

--- app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/PagerAdapter.java
+++ app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/PagerAdapter.java
@@ -21,8 +21,10 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
                 return new ReportMissingPersonFragment();
             case 1:
                 return new MissingPersonReportsFragment();
-//            case 2:
-//                return new TabFragment3();
+            case 2:
+                return new ReportCrimeFragment();
+            case 3:
+                return new CrimeReportsFragment();
             default:
                 return null;
         }
