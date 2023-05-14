11db44dedb804bb86ae9d5989f56ce1980634ca0
shahmir-ahmed <112096617+shahmir-ahmed@users.noreply.github.com>
2023-05-14 15:18:58 +0500

    Reset Password Activity Dev.
    Developing Reset Password Activity v1.1

---
+++ app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/ResetPasswordActivity.java
@@ -0,0 +1,52 @@
+package com.example.crimereporterandmissingpersonfinderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        Intent intent = getIntent();
+
+        EditText passwordEditText = findViewById(R.id.editTextNewPassword);
+        EditText confirmPasswordEditText = findViewById(R.id.editTextConfirmPassword);
+
+
+        Button resetPassword = findViewById(R.id.reset_button);
+        resetPassword.setOnClickListener(view -> {
+            String password = passwordEditText.getText().toString();
+            String confirmPassword = confirmPasswordEditText.getText().toString();
+
+            if (password.isEmpty()) {
+                passwordEditText.setError("Password is required.");
+                passwordEditText.requestFocus();
+            }
+            else if (confirmPassword.isEmpty()) {
+                confirmPasswordEditText.setError("Confirm password is required.");
+                confirmPasswordEditText.requestFocus();
+            }
+            else if (!password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[_@#$%^&+=])(?=\\S+$).{8,}$")) {
+                passwordEditText.setError("Password should be a combination of integers, special characters, uppercase, and lowercase, and the minimum password length should be eight.");
+                passwordEditText.requestFocus();
+            }
+            else if (!password.equals(confirmPassword)) {
+                confirmPasswordEditText.setError("Passwords do not match.");
+                confirmPasswordEditText.requestFocus();
+            }
+            else {
+                Toast.makeText(this, "Record stored successfully.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
