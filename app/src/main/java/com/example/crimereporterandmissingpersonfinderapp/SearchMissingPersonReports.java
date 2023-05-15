6d45700e496e74c0ed6d27ca2b4a3b70169e6080
shahmir-ahmed <112096617+shahmir-ahmed@users.noreply.github.com>
2023-05-15 19:43:39 +0500

    Search Missing Person Reports Activity Dev.
    Developing Search Missing Person Reports Activity v1.1

---
+++ app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/SearchMissingPersonReports.java
@@ -0,0 +1,14 @@
+package com.example.crimereporterandmissingpersonfinderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchMissingPersonReports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_missing_person_reports);
+    }
+}
