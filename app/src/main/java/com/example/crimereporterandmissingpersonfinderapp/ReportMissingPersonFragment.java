3f17c67360219602787cb7f99aeb8999c9310539
shahmir-ahmed <112096617+shahmir-ahmed@users.noreply.github.com>
2023-05-20 12:40:50 +0500

    Report Missing Person Fragment Dev.
    Developing Report Missing Person Fragment v1.4 (User Dashboard)

--- app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/ReportMissingPersonFragment.java
+++ app/src/main/java/com/example/crimereporterandmissingpersonfinderapp/ReportMissingPersonFragment.java
@@ -24,6 +24,9 @@ public class ReportMissingPersonFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    // UI Controls
+
+
     public ReportMissingPersonFragment() {
         // Required empty public constructor
     }
@@ -53,6 +56,11 @@ public class ReportMissingPersonFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        // Custom logic here
+
+
+
     }
 
     @Override
